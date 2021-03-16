28ee58073717b1d5121f22b306108e6a0853c4bd
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2021-03-16 20:33:51 +0200

    First test fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LandingPage from "./components/landingPage";
 
 function App() {
+  const [data, setData] = useState([]);
   const [darkTheme, setDarkTheme] = useState(
     localStorage.darkTheme === "true" ||
       (localStorage.darkTheme === undefined &&
         window.matchMedia("(prefers-color-scheme: dark)").matches)
   );
+
   const themeChange = () => {
     localStorage.darkTheme = !darkTheme;
     setDarkTheme(!darkTheme);
   };
+
+  const getTransactions = async () => {
+    const response = await fetch("https://finanzer.normans.co.za/transactions");
+    const data = await response.json();
+    return data;
+  };
+
+  useEffect(() => {
+    getTransactions().then((data) => setData(data));
+  }, []);
 
   return (
     <div className={"App" + (darkTheme ? " dark" : "")}>
