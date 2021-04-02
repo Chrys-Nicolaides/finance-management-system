3a97444b74ac9ef4bfbe9264cc61f76035c445d7
Cameron Norman <cameronrossnorman@gmail.com>
2021-04-02 09:17:05 +0200

    refactor: Made index and app files handle auth0 better\

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import Auth0ProviderWithHistory from "./auth/auth0ProviderWithHistory";
+
 import "./index.css";
 import "./App.css";
 import App from "./App";
-import LoginButton from "./components/auth-components/LoginButton";
-import LogoutButton from "./components/auth-components/LogoutButton";
-import SignUpButton from "./components/auth-components/SignUpButton";
-import Profile from "./components/auth-components/Profile";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Auth0ProviderWithHistory
-      // domain={process.env.REACT_APP_YOUR_DOMAIN}
-      // clientId={process.env.REACT_APP_YOUR_CLIENT_ID}
-      // redirectUri={"http://localhost:3000/dashboard"}
-      // audience="https://finanzer.normans.co.za"
-      // scope="read:current_user update:current_user_metadata"
-      >
-        <LoginButton />
-        <LogoutButton />
-        <SignUpButton />
-        <App />
-
-        <Profile />
-      </Auth0ProviderWithHistory>
-    </Router>
+    <Auth0Provider
+      domain={process.env.REACT_APP_YOUR_DOMAIN}
+      clientId={process.env.REACT_APP_CLIENT_ID}
+      audience={process.env.REACT_APP_AUDIENCE}
+      redirectUri={`${window.location.origin}/dashboard`}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
