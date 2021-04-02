d2b781eec51e9f617d34ba631ca9b59d1523233b
Cameron Norman <cameronrossnorman@gmail.com>
2021-04-02 09:16:25 +0200

    feat: moved api interactions to dedicated service files

--- src/services/api/Request.js
+++ src/services/api/Request.js
@@ -1,26 +1,17 @@
-// headers
-import { useAuth0 } from "@auth0/auth0-react";
-
 export const request = async (method, path, body, headers) => {
-  // const { getAccessTokenSilently } = useAuth0();
-
   headers.set("Content-Type", "application/json");
 
-  // const accessToken = await getAccessTokenSilently({
-  //   audience: `https://finanzer.normans.co.za`,
-  //   scope: "read:current_user",
-  // });
-
   const baseUrl = "https://finanzer.normans.co.za";
-  const fetchOptions = { method, headers, body: JSON.stringify(body) };
+  let fetchOptions = {}
+
+  if (method === "GET") {
+    fetchOptions = { method, headers };
+  } else {
+    fetchOptions = { method, headers, body: JSON.stringify(body) };
+  }
+
   const url = `${baseUrl}${path}`;
-  const response = await fetch(url, {
-    ...fetchOptions,
-    headers: {
-      ...fetchOptions.headers,
-      // Authorization: `Bearer ${accessToken}`,
-    },
-  });
+  const response = await fetch(url, fetchOptions);
 
   const validStatuses = [200, 204, 304];
   const invalidStatuses = [400, 404];
